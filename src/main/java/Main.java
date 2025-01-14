ade6b84630f128d14e00d876dd84feee64e76076
Rodrigo <05rotapi@gmail.com>
2025-01-14 10:55:09 +0100

    estructura

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@
  * @date 14 enero, 2025
  */
 public class Main {
+    //El proceso hijo recibirá del padre la vocal que tiene que contar y el fichero
     public static void main(String[] args) {
-        System.out.println("prueba");
-    }
+        SumaVocales ej = new SumaVocales();
+
+        ej.iniciarProcesosHijo();
+        }
 }
---
+++ src/main/java/SumaVocal.java
@@ -0,0 +1,7 @@
+/**
+ * @author Rodrigo
+ * @date 14 enero, 2025
+ */
+public class SumaVocal {
+
+}
